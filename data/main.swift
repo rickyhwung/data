1ae4bf5119b8b72c21c52fdcb595545884b12662
Hwung Ricky <rickyhwung@gmail.com>
2015-10-21 20:12:34 +0800

    Initial Commit

---
+++ data/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  data
+//
+//  Created by RickyHwung on 15/10/21.
+//  Copyright © 2015年 RickyHwung. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
